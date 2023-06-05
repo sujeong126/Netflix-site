ac924b708804934dd43a856ea2b13ad01ab1b71e
sujeong126 <hsjhsj8010@naver.com>
2023-06-06 04:04:46 +0900

    스크립트 파일 삭제

--- script.js
+++
@@ -1,8 +0,0 @@
-$('.btn-open').click(function(){
-  $('.modal-login').fadeIn();
-  $('body').addClass('active');
-});
-$('.btn-close').click(function(){
-  $('.modal-login').fadeOut();
-  $('body').removeClass('active');
-});
